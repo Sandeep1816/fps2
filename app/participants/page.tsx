9b885fe4ac628acc6c5c4117a77013ec79563f7a
maxpo-dev <dev.maxpo@gmail.com>
2025-07-17 10:43:31 +0530

    speaker pvsm updated'

--- app/participants/page.tsx
+++ app/participants/page.tsx
@@ -81,6 +81,7 @@ export default function ParticipantsPage() {
     { src: "/image/exhibitor/NEWD.png", alt: "NEWD" },
     { src: "/image/partners/SBC.png", alt: "SBC" },
     {src:"/image/speakersCompanyLogo/DarkFilled.png",alt:"DarkFilled"},
+    {src:"/image/speakersCompanyLogo/PRVSM.png",alt:"PRVSM"},
   ];
 
   return (
